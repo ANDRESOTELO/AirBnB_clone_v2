9456c91a218d6140582b6079f738d065160c7128
ANDRESOTELO <andresotelo2020@gmail.com>
2021-03-30 22:35:53 +0000

    Update Inherits

--- models/city.py
+++ models/city.py
@@ -3,12 +3,18 @@
 from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
-
+from os import getenv
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    state_id = Column(String(60), nullable=False, ForeignKey("states.id"))
-    name = Column(String(128), nullable=False)
-    __tablename__ = "cities"
-
+    """
+    The city class, contains state ID and name
+    Class City that inherits from BaseModel
+    """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "cities"
+        state_id = Column(String(60), nullable=False, ForeignKey("states.id"))
+        name = Column(String(128), nullable=False)
+    else:
+        state_id = ""
+        name = ""
