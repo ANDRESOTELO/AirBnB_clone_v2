873373f2869661c47c4536960377b46bc4534dda
4ions <leovalenciasantana@gmail.com>
2021-03-31 03:25:44 +0000

    Update amenity------------

--- models/amenity.py
+++ models/amenity.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, String, Table, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
          __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
