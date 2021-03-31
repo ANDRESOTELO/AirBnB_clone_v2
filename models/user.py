e53d37fa03f72c1edcb710a1eebbceab80c7997b
4ions <leovalenciasantana@gmail.com>
2021-03-31 01:55:40 +0000

    Added sql for user

--- models/user.py
+++ models/user.py
@@ -8,7 +8,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     
     if getenv('HBNB_TYPE_STORAGE') == 'db':
