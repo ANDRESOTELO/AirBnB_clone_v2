a630d5830fc995f10da5c7cb3dde0d19a0480628
4ions <leovalenciasantana@gmail.com>
2021-03-31 02:58:22 +0000

    Revert "Fixed?"
    This reverts commit f4f32643888bfd239c5d2306d520bb2958626b16.
    yes

--- models/amenity.py
+++ models/amenity.py
@@ -13,3 +13,7 @@ class Amenity(BaseModel):
         name = Column(String(128), nullable=False)
     else:
         name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
