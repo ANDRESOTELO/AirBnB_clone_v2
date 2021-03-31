cb8885a04a1b11fc3625bd02ab326af6f84bdacc
4ions <leovalenciasantana@gmail.com>
2021-03-31 03:26:14 +0000

    Revert "Update amenity------------"
    This reverts commit 873373f2869661c47c4536960377b46bc4534dda.

--- models/amenity.py
+++ models/amenity.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, String, Table, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
-class Amenity(BaseModel, Base):
+class Amenity(BaseModel):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
          __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
