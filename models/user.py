f6b11fb2a56fa71bd85ee2e92f77fa71e63e02f2
ANDRESOTELO <andresotelo2020@gmail.com>
2021-03-31 04:09:44 +0000

    Revert "UPDATE cascade delete"
    This reverts commit 14cb658c2ea87ab86a491758e54207b59a256715.

--- models/user.py
+++ models/user.py
@@ -10,7 +10,6 @@ from sqlalchemy.orm import relationship, backref
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-
     __tablename__ = 'users'
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
@@ -18,12 +17,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place",
-                              backref="user",
-                              cascade: "all, delete-orphan")
-        reviews = relationship("Review",
-                               backref="user",
-                               cascade: "all, delete-o\rphan")
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
 
     else:
         email = ''
