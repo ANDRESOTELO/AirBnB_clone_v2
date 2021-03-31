daa469c0ae3951d1220b5f69fa9a2f1fa5131b9d
4ions <leovalenciasantana@gmail.com>
2021-03-31 01:59:50 +0000

    Fixed some issues

--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 import models
-from models.base_model import BaseModel
 from os import getenv
+from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -17,8 +17,6 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
     else:
         email = ''
         password = ''
