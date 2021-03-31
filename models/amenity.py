5715f89bfeb3c0bc035febb18c1874862f4c3920
4ions <leovalenciasantana@gmail.com>
2021-03-31 03:26:35 +0000

    Revert "Update amenity"
    This reverts commit 28b83764682d1c569830c3a9ca64fa452f9a6c09.

--- models/amenity.py
+++ models/amenity.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from models.place import Place
-from sqlalchemy import Column, String, Table, ForeignKey
-from sqlalchemy.orm import relationship
-from os import getenv
+from models.base_model import BaseModel
+
 
 class Amenity(BaseModel):
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-         __tablename__ = "amenities"
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship(
-            "Place",
-            secondary="place_amenity",
-            viewonly=False,
-            back_populates="amenities")
-    else:
-        name = ""
+    name = ""
