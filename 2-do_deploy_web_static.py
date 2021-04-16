75f4bcbb98164930ffe533ba74a4f01e9ba36b46
ANDRESOTELO <andresotelo2020@gmail.com>
2021-04-16 04:44:19 +0000

    FIXED | Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -38,47 +38,31 @@ def do_pack():
 
 def do_deploy(archive_path):
     """
-    Fabric script that distributes an archive to your web servers.
+    Fabric script that distributes an archive to your web servers
     """
+    file_path = archive_path
 
-    if not os.path.isfile(archive_path):
+    if not os.path.exists(archive_path):
         return(False)
     try:
-        versions/web_static_20170314233357.tgz
-        file_path = archive_path
         put(file_path, "/tmp/")
-
-        # Uncompress the archive to the folder
-        folder = file_path.split("/")
-
-        # Path to releases dir
+        folder = "/data/web_static/releases/" + file_path[9:-4]
+        file_name = file_path[9:]
+        folder_name = file_path[9:-4]
+        date = file_path[21:-4]
         releases_path = "/data/web_static/releases/"
 
-        # folder_name <archive filename without extension> to uncompress
-        unzip_path = "{}{}".format(releases_path, folder[1][0:-4])
-
-        # filename.tgz
-        file_name = folder[1]
-
-        # folder_name <archive filename without extension>
-        folder_name = folder[1][0:-4]
-        date = folder[1][11:-4]
-
-        # Folder to /data/web_static/releases/<archive filename without .tgz>
-        run("mkdir -p {}".format(unzip_path))
-        run("tar -xzf /tmp/{} -C {}"
-            .format(file_name, unzip_path))
+        run("mkdir -p {}".format(folder))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder))
         run("rm /tmp/{}".format(file_name))
         run("mv {}{}/web_static/* {}{}/"
-            .format(releases_path, folder_name, releases_path, folder_name))
-        run("rm -rf {}{}/web_static"
-            .format(releases_path, folder_name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current"
-            .format(unzip_path))
-
+            .format(releases_path, folder_name, releases_path, folder_name)\
+)
+        run("rm -rf {}{}/web_static".format(releases_path, folder_name))           run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder))
         print("New version deployed!")
-        return True
+
+        return (True)
 
     except BaseException:
-        return False
+        return (False)
