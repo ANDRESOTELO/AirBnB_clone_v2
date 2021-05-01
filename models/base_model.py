fdd9b4838d43ccf3f0a13a040d3bcb241a3eeb67
ANDRESOTELO <andresotelo2020@gmail.com>
2021-05-01 20:24:05 +0000

    FIXED | too many fucking errors

--- models/base_model.py
+++ models/base_model.py
@@ -23,28 +23,34 @@ class BaseModel:
     created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
+
     def __init__(self, *args, **kwargs):
         """Class constructor that inits class attributes"""
-        
-        # If kwargs is empty assign attributes
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'updated_at':
+                    kwargs['updated_at'] = (datetime.
+                                            strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f'))
+                if key == 'created_at':
+                    kwargs['created_at'] = (datetime.
+                                            strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f'))
+        kwargs.pop('__class__', None)
+        self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        self.__dict__.pop("_sa_instance_state", None)
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        dictionary = {}
+        for key, val in self.__dict__.items():
+            dictionary[key] = val
+        dictionary.pop("_sa_instance_state", None)
+        return '[{}] ({}) {}'.format(cls, self.id, dictionary)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
 from models.state import Base
 from sqlalchemy import Column, String, Integer, ForeignKey
@@ -10,7 +11,11 @@ from models.engine.file_storage import FileStorage
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """ State class
+    Attributes:
+    name = input name
+    cities = relationship between state table and city table"""
+
     __tablename__ = "states"
 
     # HBNB_TYPE_STORAGE = The type of storage used.
@@ -20,19 +25,20 @@ class State(BaseModel, Base):
         # Delete cascade indicates that when a parent object is marked
         # for deletion, it's related child objects should be marked too
         # https://docs.sqlalchemy.org/en/13/orm/cascades.html#cascade-delete
-        cities = relationship("City", back_populates="state", cascade="all, delete")
+        cities = relationship("City", back_populates="state",
+                              cascade="all, delete, delete-orphan")
     else:
         name = ""
 
-    # property decorator to define getter
-    @property
-    def cities(self):
-        """ Getter that return a list of City instances """
-        # <all> method in file_storage.py returns the dictionary .__objects
-        # the __objects contains all created instances
-        obj_dictionary = FileStorage.all()
-        city_instances = []
-        for key, value in obj_dictionary.items():
-            if value.state_id == self.id:
-                city_instances.append(value)
-        return (city_instances)
+        # property decorator to define getter
+        @property
+        def cities(self):
+            """ Getter that return a list of City instances """
+            # <all> method in file_storage.py returns the dictionary .__objects
+            # the __objects contains all created instances
+            obj_dictionary = models.storage.all(City)
+            city_instances = []
+            for key, value in obj_dictionary.items():
+                if value.state_id == self.id:
+                    city_instances.append(value)
+            return (city_instances)
