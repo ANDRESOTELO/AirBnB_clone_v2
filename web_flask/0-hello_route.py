f5ee47f3dbb9b062f044611f3d901f4e22e04a6a
ANDRESOTELO <andresotelo2020@gmail.com>
2021-05-01 05:45:06 +0000

    Flask web framework && git push

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# First you have to import Flask class.
+from flask import Flask
+
+# An instance of Flask class, it will be our application
+# First argument is the name of the package or module
+app = Flask(__name__)
+
+
+# decorator is used to tell Flask what URL should trigger our function
+@app.route('/', strict_slashes=False)
+def hello_flask():
+    """
+    Function to stars a Flask web application and return a message
+    """
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    # Call run method of our instance, and defines host and port
+    app.run(host="0.0.0.0", port=5000)
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# First you have to import Flask class.
+from flask import Flask
+
+# An instance of Flask class, it will be our application
+# First argument is the name of the package or module
+app = Flask(__name__)
+
+
+# decorator is used to tell Flask what URL should trigger our function
+@app.route('/', strict_slashes=False)
+def hello_flask():
+    """
+    Function to stars a Flask web application and return a message
+    """
+    return 'Hello HBNB!'
+
+
+# Routing
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_flask():
+    """
+    Defines other route - Routing -
+    """
+    return 'HBNB'
+
+
+# Variable routes
+@app.route('/c/<text>', strict_slashes=False)
+def variable_routes(text):
+    """
+    You can add variable sections to a URL by marking sections with
+    <variable_name>. Your function then receives the <variable_name>
+    as a keyword argument
+    """
+    return ('C %s' % (text))
+
+
+if __name__ == '__main__':
+    # Call run method of our instance, and defines host and port
+    app.run(host="0.0.0.0", port=5000)
