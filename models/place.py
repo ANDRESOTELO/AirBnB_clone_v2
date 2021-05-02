a884f38d20774aa683c777b4a0d4370ef246c425
ANDRESOTELO <andresotelo2020@gmail.com>
2021-05-02 20:25:04 +0000

    FIXED | pep8 style

--- models/place.py
+++ models/place.py
@@ -10,7 +10,7 @@ from os import getenv
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    
+
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
--- models/review.py
+++ models/review.py
@@ -11,7 +11,7 @@ class Review(BaseModel, Base):
     """ Review classto store review information """
 
     __tablename__ = 'reviews'
-    
+
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
